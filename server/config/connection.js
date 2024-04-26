bd4054ae40c2ac89ece677c7baca91f34e57842f
Aditya Bhagat <113513123+Aditya-Bhagat03@users.noreply.github.com>
2024-04-27 01:29:01 +0530

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,14 +2,12 @@ const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", false);
 
-mongoose.connect("mongodb+srv://adi:ola@todo.you0jbe.mongodb.net/?retryWrites=true&w=majority&appName=todo"{
+mongoose.connect("mongodb+srv://adi:ola@todo.you0jbe.mongodb.net/?retryWrites=true&w=majority&appName=todo", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }, err => {
   if (err) throw err;
-  console.log('Connected to MongoDB!')
-}
-
-);
+  console.log('Connected to MongoDB!');
+});
 
 module.exports = mongoose.connection;
