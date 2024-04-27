c8534da0df5e3b72890b4ccb081eadbe0b4a3203
Aditya Bhagat <113513123+Aditya-Bhagat03@users.noreply.github.com>
2024-04-27 12:09:56 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === "production") {
 
 // Serve index.html for all routes in production
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../opt/render/project/src/index.html'));
+  res.sendFile(path.join(__dirname, '../opt/render/project/src/server.js'));
 });
 
 // Register API routes
