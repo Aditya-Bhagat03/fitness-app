40beed02653f27ece42be9bb8fbb4e745fedacff
Aditya Bhagat <113513123+Aditya-Bhagat03@users.noreply.github.com>
2024-04-27 10:43:52 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,9 +3,10 @@ const path = require("path");
 const routes = require("./routes");
 const db = require("./config/connection");
 
-const PORT =3002;
+const PORT = 3002;
 const app = express();
 
+// Middleware to parse incoming request bodies
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -14,13 +15,17 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
+// Serve index.html for all routes in production
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
+// Register API routes
 app.use(routes);
 
+// Connect to the database
 db.once("open", () => {
+  // Start the server
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
   });
